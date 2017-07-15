2c4998a07590bdffe295cc5d78dbbb920a4debe4
Alex Geer <monoflash@gmail.com>
2017-07-15 22:13:09 +0300

    Added example

--- cpy/cpy_example_test.go
+++ cpy/cpy_example_test.go
@@ -11,7 +11,7 @@ import (
 	"gopkg.in/webnice/cpy.v1/cpy"
 )
 
-// Copying all data without filtration with type conversion and field matching.
+// Copying all data without filtration with type conversion and field matching
 func ExampleAll_everything() {
 	// Src Source structure
 	type Src struct {
@@ -69,6 +69,63 @@ func ExampleAll_everything() {
 	//     "title": "Isabella",
 	//     "des": "Brown",
 	//     "crateAt": "2017-07-15 10:35:24 +0000 UTC"
+	//   }
+	// ]
+}
+
+// Copying slice with filtration function
+func ExampleFilter_filtration() {
+	// Src Source structure
+	type Src struct {
+		ID       int64
+		FullName string
+		Age      int32
+	}
+
+	// Dst Destination structure
+	type Dst struct {
+		NewID int     `cpy:"name=ID"       json:"id"`    // Overriding the field name to match the field name in the data source structure
+		Title *string `cpy:"name=FullName" json:"title"` // Overriding the field name to match the field name in the data source structure
+	}
+
+	var source []Src
+	var destination []*Dst
+
+	source = []Src{
+		Src{ID: 1, FullName: "Aiden Smith", Age: 17},
+		Src{ID: 2, FullName: "Liam Johnson", Age: 19},
+		Src{ID: 3, FullName: "Isabella Brown", Age: 21},
+	}
+
+	// Сopy everything from one to another
+	err := cpy.Filter(&destination, &source, func(key interface{}, object interface{}) (skip bool) {
+		skip = true // By default all rows are skipped
+		// This is filtration function
+		// key is index of value in slice and key in map (In this example is not required)
+		if v, ok := object.(Src); ok {
+			// filter by age >= 18
+			if v.Age >= 18 {
+				skip = false
+			}
+		}
+		return
+	})
+	if err != nil {
+		log.Fatalf("Error copy: %s", err.Error())
+	}
+	// Output result
+	b, _ := json.MarshalIndent(destination, "", "  ")
+	fmt.Printf("%s\n", string(b))
+
+	// Output:
+	// [
+	//   {
+	//     "id": 2,
+	//     "title": "Liam Johnson"
+	//   },
+	//   {
+	//     "id": 3,
+	//     "title": "Isabella Brown"
 	//   }
 	// ]
 }
